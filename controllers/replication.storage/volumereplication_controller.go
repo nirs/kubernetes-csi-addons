nirs/kubernetes-csi-addons

commit 90f59d1f113e1c7f4dc8f308d18ef213ec887a4e

controller/replication: add UnimplementedError for getVolRepInfo()
This commit adds code to handle UnimplmentedError from
getVolumeReplicationInfo rpc. The controller wil now
ignore this error and not requeue the request.

Fixes: #236

Signed-off-by: Rakshith R <rar@redhat.com>

controllers/replication.storage/volumereplication_controller.go
@@ -371,20 +371,22 @@ func (r *VolumeReplicationReconciler) Reconcile(ctx context.Context, req ctrl.Re
 
 	instance.Status.LastCompletionTime = getCurrentTime()
 
-	var requeueForInfo bool
+	requeueForInfo := false
 
 	if instance.Spec.ReplicationState == replicationv1alpha1.Primary {
 		info, err := r.getVolumeReplicationInfo(vr)
-		if err != nil {
+		if err == nil {
+			ts := info.GetLastSyncTime()
+			if ts != nil {
+				lastSyncTime := metav1.NewTime(ts.AsTime())
+				instance.Status.LastSyncTime = &lastSyncTime
+			}
+			requeueForInfo = true
+		}
+		if !util.IsUnimplementedError(err) {
 			logger.Error(err, "Failed to get volume replication info")
 			return ctrl.Result{}, err
 		}
-		ts := info.GetLastSyncTime()
-		if ts != nil {
-			lastSyncTime := metav1.NewTime(ts.AsTime())
-			instance.Status.LastSyncTime = &lastSyncTime
-		}
-		requeueForInfo = true
 	}
 	if instance.Spec.ReplicationState == replicationv1alpha1.Secondary {
 		instance.Status.LastSyncTime = nil

internal/util/error.go
@@ -16,7 +16,10 @@ limitations under the License.
 
 package util
 
-import "google.golang.org/grpc/status"
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 // GetErrorMessage returns the message from the error if it is a grpc error,
 // else returns err.Error().
@@ -27,4 +30,14 @@ func GetErrorMessage(err error) string {
 	}
 
 	return s.Message()
+}
+
+// IsUnimplementedError returns true if the error is Unimplemented error.
+func IsUnimplementedError(err error) bool {
+	s, ok := status.FromError(err)
+	if !ok {
+		return false
+	}
+
+	return s.Code() == codes.Unimplemented
 }

internal/util/error_test.go
@@ -63,3 +63,48 @@ func TestGetErrorMessage(t *testing.T) {
 		})
 	}
 }
+
+func TestIsUmplementedError(t *testing.T) {
+	type input struct {
+		err error
+	}
+	tests := []struct {
+		name string
+		args input
+		want bool
+	}{
+		{
+			name: "nil error",
+			args: input{
+				err: nil,
+			},
+			want: false,
+		},
+		{
+			name: "Unimplmented error",
+			args: input{
+				err: status.Error(codes.Unimplemented, "unimplemented"),
+			},
+			want: true,
+		},
+		{
+			name: "Non unimplmented error",
+			args: input{
+				err: status.Error(codes.NotFound, "not found"),
+			},
+			want: false,
+		},
+		{
+			name: "Non grpc status error",
+			args: input{
+				err: errors.New("new error"),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, IsUnimplementedError(tt.args.err))
+		})
+	}
+}

commit 3528b6b57cd05a00fd32cb27be82fb7001214b62

Fixes error handling of getVolRepInfo() result
Successful RPC ended in an error since nil is not an unimplemented
error.

Fixes: 90f59d1f113e
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

controllers/replication.storage/volumereplication_controller.go
@@ -382,10 +382,11 @@ func (r *VolumeReplicationReconciler) Reconcile(ctx context.Context, req ctrl.Re
 				instance.Status.LastSyncTime = &lastSyncTime
 			}
 			requeueForInfo = true
-		}
-		if !util.IsUnimplementedError(err) {
-			logger.Error(err, "Failed to get volume replication info")
-			return ctrl.Result{}, err
+		} else {
+			if !util.IsUnimplementedError(err) {
+				logger.Error(err, "Failed to get volume replication info")
+				return ctrl.Result{}, err
+			}
 		}
 	}
 	if instance.Spec.ReplicationState == replicationv1alpha1.Secondary {